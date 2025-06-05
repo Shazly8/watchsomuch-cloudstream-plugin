292095962f045288dfab70376b1bb74905702ba4
Shazly8 <119331416+Shazly8@users.noreply.github.com>
2025-06-06 01:06:20 +0300

    Create build.gradle.kts

---
+++ WatchSoMuchProvider/src/main/kotlin/com/watchsomuch/WatchSoMuchProvider/build.gradle.kts
@@ -0,0 +1,20 @@
+buildscript {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.0.4")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10")
+        classpath("com.github.recloudstream:gradle:master-SNAPSHOT")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
+}
